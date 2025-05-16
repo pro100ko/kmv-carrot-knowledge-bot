e44de9b0c42f60b235d35f93dadc539a5e670dfe
pro100ko <111150165+pro100ko@users.noreply.github.com>
2025-05-16 14:28:48 +0300

    Update user_management.py

--- handlers/user_management.py
+++ handlers/user_management.py
@@ -2,7 +2,7 @@
 from aiogram import types
 from aiogram.enums import ParseMode
 
-import sqlite_db import get_test_attempts
+from sqlite_db import get_test_attempts
 from config import ADMIN_IDS
 from utils.keyboards import get_main_keyboard
 
