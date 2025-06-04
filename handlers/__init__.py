86cb1606aa84ad3a0707ef95b7904a51debb50af
Dmitrij Kataev <dmitrijkataev@gmail.com>
2025-06-04 10:15:04 +0300

    Correct handler registration function imports in __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,4 @@
-from .user import register_user_handlers
-from .catalog import register_catalog_handlers
-from .tests import register_test_handlers
-from .admin import register_admin_handlers
+from .user import setup_user_handlers as register_user_handlers
+from .catalog import setup_catalog_handlers as register_catalog_handlers
+from .tests import setup_test_handlers as register_test_handlers
+from .admin import setup_admin_handlers as register_admin_handlers
